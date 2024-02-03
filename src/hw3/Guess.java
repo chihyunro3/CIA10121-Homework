800e172ddcf9109bf579ab70bbad45d1b2992812
ChihYun Wu <wu20603@gmail.com>
2024-02-04 01:17:48 +0800

    optimize HW3-Q2

--- src/hw3/Guess.java
+++ src/hw3/Guess.java
@@ -6,18 +6,18 @@ public class Guess {
 
 	public static void main(String[] args) {
 
-//		取亂數1 ~ 9
-		int r1 = (int) (Math.random() * 99) + 1; // 總共有10個數，+ 邊界最小數值
+//		取亂數0 ~ 100
+		int r1 = (int) (Math.random() * 101) + 0; // 總共有101個數，+ 邊界最小數值
 
 //		猜數字，宣告一個猜數字的物件
-		int i;
 		Scanner sc = new Scanner(System.in);
+		
 
-//			System.out.print(i);
+//		System.out.print(i);
 
-//		顯示訊息
+//		顯示提示訊息
 		System.out.println("開始猜數字吧");
-		i = sc.nextInt();
+		int i = sc.nextInt();
 		while (r1 != i) {
 			if (r1 < i) {
 				System.out.println("猜錯囉！比你猜的小！");
